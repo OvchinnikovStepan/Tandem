9205b008ca105c8fa2e062045f720f782da98d64
Erlan <erlanchuacbaev@gmail.com>
2025-12-18 17:47:15 +0600

    refactor: update pages

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./src/pages/Login";
 import Register from "./src/pages/Register";
+import ForgotPassword from "./src/pages/ForgotPassword";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
   );
