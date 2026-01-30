a4a9606554074281b4668800dfe3e5ac2c0aa32d
korolevaka <163682991+korolevaka@users.noreply.github.com>
2026-01-30 16:40:20 +0600

    refactor: combine migrations into one script

--- backend/profile_service/src/main/resources/db/migration/V3__Create_polls_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE polls (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    version INTEGER NOT NULL,
-    is_active BOOLEAN DEFAULT TRUE,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    UNIQUE(name, version)
-);
