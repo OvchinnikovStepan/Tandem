0ec646e89612919b4485d14c2f2f1f852f1c1a85
Erlan <erlanchuacbaev@gmail.com>
2026-02-05 11:56:35 +0600

    chore: rm pagination export

--- frontend/src/modules/ProfileContent/index.ts
+++ frontend/src/modules/ProfileContent/index.ts
@@ -2,7 +2,6 @@ export { default as ProfileHeader } from "./components/ProfileHeader/ProfileHead
 export { default as ProfileInfo } from "./components/ProfileInfo/ProfileInfo";
 export { default as ProfileTabs } from "./components/ProfileTabs/ProfileTabs";
 export { default as PostsGrid } from "./components/PostsGrid/PostsGrid";
-export { default as Pagination } from "./components/Pagination/Pagination";
 export { default as ProfileEdit } from "./components/ProfileEdit/ProfileEdit";
 export { default as ProfileEditInterests } from "./components/ProfileEditInterests/ProfileEditInterests";
 export { default as ProfileEditCareer } from "./components/ProfileEditCareer/ProfileEditCareer";
