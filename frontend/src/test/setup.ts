b22395eddde3df440384dfd887640c819036ac50
Erlan <erlanchuacbaev@gmail.com>
2026-02-19 14:55:49 +0600

    chore: init i18n

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+import "@/i18n/i18n";
+import i18n from "@/i18n/i18n";
+
+beforeEach(async () => {
+	localStorage.setItem("language", "ru");
+	await i18n.changeLanguage("ru");
+});
